ae58e82434ee8f66d36a8066ebc50eec6c28150c
krq33 <krq33@drexel.edu>
2024-12-12 01:05:11 -0500

    completed functions, added UI

--- RiggingUI_v01.py
+++ RiggingUI_v01.py
@@ -6,74 +6,99 @@ assetName = str(os.getenv('ASSET_NAME'))
 
 print(assetName)
 
-#define function that generates a group based on env variable asset name (GRP_<ASSET>)
-def createGroup():    
-    groupName = ('GRP_'+ assetName)
+#assign selected object to variable (add code that yells at user if they have nothing selected)
+selectedObject = cmds.ls(sl=True)
+
+#UI STUFF!!!
+def createWin():
+    #check if a window already exists. If yes, delete it
+    if cmds.window('rigObject', exists=True):
+        cmds.deleteUI('rigObject')
     
-    #check if the group already exists    
-    if cmds.objExists(groupName):
-        print('This group already exists dumbass')
-        return
-    #if not, continue...    
-    else: 
-        selectedObject = cmds.ls(sl=True)
-        
-        #create GRP_<ASSET> group
-        grp_MASTER= cmds.group( em=True, name=(groupName))    
-     
-        #create child groups GRP_geom and GRP_rig
-        grp_GEO= cmds.group(em= True, name='GRP_geo')
-        grp_RIG= cmds.group(em= True, name='GRP_rig')
-        cmds.parent(grp_GEO, grp_MASTER)
-        cmds.parent(grp_RIG, grp_MASTER)
-        cmds.parent(selectedObject, grp_GEO)
-
-createGroup()
-
-
-#define a function that generates three locators (probably with slight offsets)
-#name appropriately
-def placeLocators():
-    locRoot = cmds.spaceLocator(p=(0,0,0), n='LOC_root')
-    locBase = cmds.spaceLocator(p=(0,1,0), n='LOC_base')
-    locMove = cmds.spaceLocator(p=(0,2,0), n='LOC_move')
+    #create a new window
+    cmds.window('rigObject', title='Rig Object', widthHeight=(300, 100))
+    #define the layout of the window
+    cmds.columnLayout(adjustableColumn=True)
     
-    #function that generates joints at locator locations
-    def placeJoints(locRoot, locBase, locMove):          
-        position = cmds.xform(locRoot, query=True, ws=True, translation=True)
-        rootJoint = cmds.joint(p=position)
-        cmds.parent(rootJoint, world=True)
-        cmds.rename(rootJoint, 'Root_jnt')
+    #define function that generates a group based on env variable asset name (GRP_<ASSET>)
+    def createGroup(*args):    
+        groupName = ('GRP_'+ assetName)
         
-        position = cmds.xform(locBase, query=True, ws=True, translation=True)
-        baseJoint = cmds.joint(p=position)      
-        #cmds.parent(baseJoint, rootJoint)
-        cmds.rename(baseJoint, 'Base_jnt')
-        
-        position = cmds.xform(locMove, query=True, ws=True, translation=True)
-        moveJoint = cmds.joint(p=position)
-        #cmds.parent(moveJoint, baseJoint)
-        cmds.rename(moveJoint, 'Move_jnt')
-        
-    #JOINTS NOT GENERATING AT LOCATOR POSITIONS, FIX!
-    placeJoints(locRoot, locBase, locMove)
+        #check if the group already exists    
+        if cmds.objExists(groupName):
+            print('This group already exists dumbass')
+            cmds.confirmDialog(title='wanh wanhh', message='This group already exists.')
+            return
+        #if not, continue...    
+        else:
+            #create GRP_<ASSET> group
+            grp_MASTER= cmds.group( em=True, name=(groupName))    
+         
+            #create child groups GRP_geom and GRP_rig
+            grp_GEO= cmds.group(em= True, name='GRP_geo')
+            grp_RIG= cmds.group(em= True, name='GRP_rig')
+            cmds.parent(grp_GEO, grp_MASTER)
+            cmds.parent(grp_RIG, grp_MASTER)
+            cmds.parent(selectedObject, grp_GEO)
+            
+        #define locator generator function
+        def placeLocators(*args):
+            locRoot = cmds.spaceLocator(p=(0,0,0), n='LOC_root')
+            cmds.xform(locRoot, centerPivots=True)
+            
+            locBase = cmds.spaceLocator(p=(0,0,0), n='LOC_base')
+            cmds.xform(locBase, centerPivots=True)
+            
+            locMove = cmds.spaceLocator(p=(0,0,0), n='LOC_move')
+            cmds.xform(locMove, centerPivots=True)
 
-
-placeLocators()
-   
-#define a function that creates a joint hierarchy using the locators as reference
-    #check if joints already exist for this mesh/meshes
-        #if yes
-            #prompt the user "Are you sure you want to move joints to current locator positions?" y/n
-            #move existing joints to locators            
+            #define joint generator function
+            def placeJoints(*args):          
+                position = cmds.xform(locRoot, query=True, worldSpace=True, translation=True)
+                print(position)
+                rootJoint = cmds.joint(p=position)
+                cmds.parent(rootJoint, world=True)
+                cmds.rename(rootJoint, 'Root_jnt')
+                
+                position = cmds.xform(locBase, query=True, worldSpace=True, translation=True)
+                print(position)
+                baseJoint = cmds.joint(p=position)      
+                cmds.rename(baseJoint, 'Base_jnt')
+                
+                position = cmds.xform(locMove, query=True, worldSpace=True, translation=True)
+                print(position)
+                moveJoint = cmds.joint(p=position)
+                cmds.rename(moveJoint, 'Move_jnt')
+                
+                #define a function that binds the mesh to the rig
+                def bindSelected(*args):
+                    cmds.bindSkin(selectedObject, jointChain, ta=True)
+                    print("Skin bind successful.")
+        
+                #select all children of root joint
+                jointChain = cmds.listRelatives('Root_jnt', ad=True)
+                print(jointChain)
+                
+                #"bind skin" button
+                cmds.button('Bind rig to selected.', command=bindSelected)
+                print('DONE!')
+            
+            #"create joints" button    
+            cmds.button(label='Create joints at locator positions.', command=placeJoints)
+            #debugging print statement
+            print('create joints?')
+        
+        #"Generate locators" button
+        cmds.button(label='Generate locators.', command=placeLocators)
+    
+    #"Generate group" button
+    cmds.button(label='Generate group.', command=createGroup) 
+    
+    cmds.showWindow('rigObject')
+    
+createWin()
 
 """
-#define a function that binds the mesh to the rig
-def bindSkin(*args):
-    
-    #prompt the user "Are you sure you want to bind skin? y/n
-    
-
 #connect functions to buttons...
 win = cmds.window(title="Procedural Rigging Tool", widthHeight=(200,125)) 
 layout =cmds.columnLayout()
